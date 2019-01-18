42a3c2fdabd76a9b9ff825373be360d5f07aa45e
Alex ofobrukweta <alexgraphicspro@yahoo.com>
2019-01-19 00:13:00 +0100

    My first HomeWork

---
+++ main.py
@@ -0,0 +1,13 @@
+Artist = "Sinach"
+Song_Title = "I know who I am"
+Album_tile = "I am Blessed"
+Genre = "Gospel"
+Duration_in_minutes = 1.52
+Year_Released = 2000
+
+print(Artist)
+print(Song_Title)
+print(Album_tile)
+print(Genre)
+print(Duration_in_minutes)
+print(Year_Released)
